aab9ee17b6903b9188632149d18b149aaacab338
Karen Melo <karen_melo@live.com>
2022-09-18 11:52:58 -0300

    modificado o schematics para nao gerar template inline.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
