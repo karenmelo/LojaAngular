0ebbb6aa3b6b4d13ca137b370e855f2c9520d2f7
Karen Melo <karen_melo@live.com>
2022-10-01 00:56:45 -0300

    Incluido o component demos para testar os data bindings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { SobreComponent } from './institucional/sobre/sobre.component';
-import { ContatoComponent } from './institucional/contato/contato.component';
-import { HomeComponent } from './navegacao/home/home.component';
+import { DataBindingComponent } from "./demos/data-binding/data-binding.component";
+import { SobreComponent } from "./institucional/sobre/sobre.component";
+import { ContatoComponent } from "./institucional/contato/contato.component";
+import { HomeComponent } from "./navegacao/home/home.component";
 import { Routes } from "@angular/router";
 
 export const rootRouteConfig: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'contato', component: ContatoComponent},
-    {path: 'sobre', component: SobreComponent}
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "contato", component: ContatoComponent },
+  { path: "sobre", component: SobreComponent },
+  { path: "feature-data-binding", component: DataBindingComponent },
 ];
---
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-data-binding",
+  templateUrl: "./data-binding.component.html",
+  styles: [],
+})
+export class DataBindingComponent {
+  public contadorClique: number = 0;
+  public urlImage: string = "";
+}
